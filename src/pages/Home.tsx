ff59922adfd152e448a2b5b64e027d9165084c86
Andres <andresalvarez360@gmail.com>
2024-11-27 16:49:05 -0300

    agregando paginas Landpage, contacto, about y terminos y condicones

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from "../components/Carousel.tsx";
+
+const Home: React.FC = () => {
+    return (
+        <div className="border-gray-500">
+            <Carousel />
+        </div>
+    );
+};
+
+export default Home;
